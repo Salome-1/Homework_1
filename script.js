cd685c245b173748ecbeaa3f00737cb9ea01f47d
Salome kekelia <salome.kekelia1@gmail.com>
2023-04-29 23:40:02 +0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+//Homework_1
+
+let number = 100;
+
+if (number < 50) {
+  console.log("Less than 50");
+} else if (number > 20) {
+  console.log("More than 20");
+} else {
+  console.log("error");
+}
+
+// Homework_2
+
+let nameUser = "mariam";
+
+if (nameUser == "mariam") {
+  console.log("true");
+} else {
+  console.log("error");
+}
+
+// Homework_3
+
+switch (nameUser) {
+  case "Mariam":
+    console.log("true");
+    break;
+  default:
+    console.log("error");
+}
